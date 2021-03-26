aa5450bf58f74697daa05a0e77a0a371336fbe38
whtvrkaa <whtvrka@gmail.com>
2021-03-26 20:09:04 +0000

    first

---
+++ components/Bird.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {View} from 'react-native';
+
+const Bird = () => {
+return (
+    <View style={{position: 'absolute',
+    backgroundColor: 'blue',
+    width: 50,
+    height: 60,}}/>
+
+
+)
+}
+
+export default Bird;
